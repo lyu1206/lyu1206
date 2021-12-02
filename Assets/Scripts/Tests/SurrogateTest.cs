e58a6d629b91a3d73cc0b20cc3527bf3072c578f
류충열 <lyu1206@naver.com>
2021-12-02 22:39:43 +0900

    runtime editor 게임뷰 화이트화 오류 수정

--- Assets/Scripts/Tests/SurrogateTest.cs
+++ Assets/Scripts/Tests/SurrogateTest.cs
@@ -17,16 +17,58 @@ using Battlehub.Utils;
 using Battlehub.RTSL;
 
 
-public class SurrogateTest : ProjectGuids
+public class SurrogateTest : Project
 {
     [SerializeField]
     private GameObject _prefab;
+    private IAssetDB<long> m_assetDB;
+
     // Start is called before the first frame update
+    protected override void Awake()
+    {
+        ITypeMap typeMap = new TypeMap<long>();
+        IOC.Register(typeMap);
+        var assetDb = new AssetDB();
+        IOC.Register<IAssetDB>(assetDb);
+        IOC.Register<IAssetDB<long>>(assetDb);
+        base.Awake();
+    }
+    private void _getUnmappedObjects(GameObject go, List<UnityObject> notMapped)
+    {
+        if (go.GetComponent<RTSLIgnore>())
+        {
+            return;
+        }
+
+        if (!m_assetDB.IsMapped(go))
+        {
+            notMapped.Add(go);
+        }
+
+        Transform[] transforms = go.GetComponentsInChildren<Transform>(true);
+        for (int i = 0; i < transforms.Length; ++i)
+        {
+            Transform tf = transforms[i];
+            if (tf.gameObject != go && !m_assetDB.IsMapped(tf.gameObject))
+            {
+                notMapped.Add(tf.gameObject);
+            }
+
+            Component[] components = tf.GetComponents<Component>();
+            for (int j = 0; j < components.Length; ++j)
+            {
+                Component comp = components[j];
+                if (!m_assetDB.IsMapped(comp))
+                {
+                    notMapped.Add(comp);
+                }
+            }
+        }
+    }
+
     void Start()
     {
-        ITypeMap typeMap = m_typeMap = new TypeMap<long>();
-        IOC.Register(typeMap);
-
+        m_assetDB = IOC.Resolve<IAssetDB<long>>();
         object obj = _prefab;
         Type objType = obj.GetType();
         Type persistentType = m_typeMap.ToPersistentType(objType);
@@ -34,9 +76,28 @@ public class SurrogateTest : ProjectGuids
         if (obj is GameObject)
         {
             GetUnmappedObjects((GameObject)obj, notMapped);
+            //            _getUnmappedObjects((GameObject)obj, notMapped);
         }
-        PersistentObject<Guid> persistentObject = (PersistentObject<Guid>)Activator.CreateInstance(persistentType);
+        var persistentObject = (PersistentObject<long>)Activator.CreateInstance(persistentType);
         persistentObject.ReadFrom(obj);
+
+
+        GenerateIdentifiers(notMapped.Count, (error, ids) =>
+        {
+            if (error.HasError)
+            {
+                //if (callback != null)
+                //{
+                //    callback(error, null);
+                //}
+
+                //ao.Error = error;
+                //ao.Result = null;
+                //ao.IsCompleted = true;
+                //done();
+                return;
+            }
+        });
     }
     // Update is called once per frame
     void Update()
